66fd0a6652983a0351e34604ecea09012ce60617
Дмитрий Кирилин <dim_dimyych@mail.ru>
2023-04-02 17:33:02 +0400

    add

--- src/test/java/projectPackageOne/ConfProperties.java
+++ src/test/java/projectPackageOne/ConfProperties.java
@@ -3,6 +3,7 @@ package projectPackageOne;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
+
 public class ConfProperties {
 
     protected static FileInputStream fileInputStream;
@@ -18,33 +19,30 @@ public class ConfProperties {
 
             PROPERTIES.load(fileInputStream);
 
-        }
-
-        catch (IOException e) {
+        } catch (IOException e) {
 
             e.printStackTrace();
 
             //обработка возможного исключения (нет файла и т.п.)
-        }
-
-        finally {
+        } finally {
             if (fileInputStream != null)
                 try {
 
                     fileInputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
-
-            catch (IOException e) {
-                    e.printStackTrace(); }
 
         }
 
     }
+
     /**
      * метод для возврата строки со значением из файла с настройками
      */
     public static String getProperty(String key) {
 
-        return PROPERTIES.getProperty(key); }
+        return PROPERTIES.getProperty(key);
+    }
 
 }
